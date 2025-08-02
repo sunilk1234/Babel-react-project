82a7f08785cc784e5a140633414521816ea32fc4
Badireddy Sunil Kumar Reddy <98638402+sunilk1234@users.noreply.github.com>
2025-08-02 15:09:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavigBar from './Components/NavigBar';
+import Body from './Components/Body';
+import Cards from './Components/Cards';
+
+
+const App = () => {
+  return (
+    <div>
+      <NavigBar/>
+      <Body/>
+      <Cards/>
+    </div>
+  )
+}
+
+export default App
